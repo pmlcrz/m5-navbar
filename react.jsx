a8968c542d83059731c2c8a234a0b6f5bcc3eda7
Pamela Cruz <117556345+pmlgcz@users.noreply.github.com>
2023-03-30 12:47:00 -0300

    Add files via upload

---
+++ react.jsx
@@ -0,0 +1,18 @@
+const react = () => {
+    return(
+        <>
+        <nav class="navbar bg-dark navbar-dark d-flex  ">
+    <div class="logo"><img src="./img/ztechLOGO.png" width="50%" height="50%"></div>
+    <div class="nav navbar-nav navbar-right">
+        <div class="d-flex mb-1 align-self-center">
+            <div> <a class="navbar-brand" href="form.html">Inicio</a></div>
+            <div> <a class="navbar-brand" href="form.html">Produtos</a></div>
+            <div> <a class="navbar-brand" href="form.html">Cadastro</a></div>
+            <div> <a class="navbar-brand" href="form.html">Sobre</a></div>
+        </div>
+    </div>
+</nav>
+        </>
+    )
+}
+export default react
